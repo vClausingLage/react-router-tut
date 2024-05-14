93fee15ef95eddfa1aef3907b26482e283729fc4
Vincent Clausing-Lage <vclausing-lage@haj.tech>
2024-05-14 21:14:27 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact'
+import EditContact, {
+  action as editAction,
+} from "./routes/edit"
+import { action as destroyAction } from "./routes/destroy"
+import ErrorPage from "./errorPage"
+import Index from './routes';
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "/contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+            errorElement: <ErrorPage />,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+            errorElement: <ErrorPage />,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <ErrorPage />,
+          }
+        ]
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
